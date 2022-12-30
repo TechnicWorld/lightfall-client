0d38cbb99491cfcb36138b212f64d771cbb9d401
BAGU5 <35269946+BAGU5@users.noreply.github.com>
2022-12-30 08:01:00 +0000

    t

--- src/main/java/com/ixnah/mc/protocol/mixin/forge/fml/network/FMLHandshakeHandlerMixin.java
+++ src/main/java/com/ixnah/mc/protocol/mixin/forge/fml/network/FMLHandshakeHandlerMixin.java
@@ -11,15 +11,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = FMLHandshakeHandler.class, remap = false)
-public class FMLHandshakeHandlerMixin {
+// @Mixin(value = FMLHandshakeHandler.class, remap = false)
+// public class FMLHandshakeHandlerMixin {
 
-    @Redirect(method = "handleServerModListOnClient", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;closeChannel(Lnet/minecraft/util/text/ITextComponent;)V"))
-    private void printInvalidChannels(NetworkManager networkManager, ITextComponent message) {
-        StringBuilder builder = new StringBuilder("Connection closed - mismatched mod channel list: ");
-        for (Pair<ResourceLocation, Boolean> result : ValidateChannelsUtil.getLatestResults()) {
-            builder.append("\n").append(result.getKey().toString());
-        }
-        networkManager.closeChannel(new StringTextComponent(builder.toString()));
-    }
-}
+//     @Redirect(method = "handleServerModListOnClient", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;closeChannel(Lnet/minecraft/util/text/ITextComponent;)V"))
+//     private void printInvalidChannels(NetworkManager networkManager, ITextComponent message) {
+//         StringBuilder builder = new StringBuilder("Connection closed - mismatched mod channel list: ");
+//         for (Pair<ResourceLocation, Boolean> result : ValidateChannelsUtil.getLatestResults()) {
+//             builder.append("\n").append(result.getKey().toString());
+//         }
+//         networkManager.closeChannel(new StringTextComponent(builder.toString()));
+//     }
+// }
